8c7325046a60c999604742491b17ce0501fbf5d1
Anshul Neekhara <anshulneekhara@gmail.com>
2023-10-12 10:41:57 +0530

    add 'sub' and 'mul' function

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,12 @@
 def sum(a,b):
     print(f'the sum of {a} and {b} is {a+b}')
 
+def sum(a,b):
+    print(f'the sum of {a} and {b} is {a+b}')
+
+def sub(a,b):
+    print(f'the difference between {a} and {b} is {a-b}')
+
+def mul(a,b):
+    print(f'the product of {a} and {b} is {a*b}')
 
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from Calculator import sum
+from Calculator import sum,sub,mul
 
 sum(2,5)
+sub(2,5)
+mul(2,5)
+
+

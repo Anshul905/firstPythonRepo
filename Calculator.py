6334dff8ef22b3adcd0ac4a509b501b4096977d9
Anshul Kumar Neekhara <86338469+Anshul905@users.noreply.github.com>
2023-10-23 12:13:01 +0000

    division method added

--- Calculator.py
+++ Calculator.py
@@ -7,3 +7,8 @@ def sub(a,b):
 def mul(a,b):
     print(f'the product of {a} and {b} is {a*b}')
 
+def div(a,b):
+    if b!=0:
+        print(f'the division of {a} and {b} is {a/b}')
+    else:
+        print('Denomerator is 0 here')
